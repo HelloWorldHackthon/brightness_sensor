470c615160e79ba086eaade15dadc7441d567e7d
Kim-Gangmin <101308701+min-524@users.noreply.github.com>
2022-12-03 07:31:14 +0900

    UPLOAD :: light

---
+++ light/light.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+
+
+// Replace with your network credentials
+const char* ssid     = "HACKATHON 5";
+const char* password = "HACK-567";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http://leejiseok.iptime.org/input3.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // (you can also pass in a Wire library object like &Wire2)
+  //bool status = bme.begin(0x76);
+  //if (!status) {
+    //Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
+    //while (1);
+  //}
+}
+
+void loop() {
+  int Light = analogRead(A0);
+  int li = map(Light, 800, 1024, 0, 100);
+  Serial.print("밝기는 ");
+  Serial.print(li);
+  Serial.println(" 입니다");
+  delay(1000);
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(client, serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // Prepare your HTTP POST request data
+    String httpRequestData = "value=" + String(li)+"";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = http.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //http.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+        
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+
+
+
+  //Send an HTTP POST request every 30 seconds
+  delay(3000);  
+}
